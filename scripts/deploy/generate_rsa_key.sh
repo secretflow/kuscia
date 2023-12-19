adb85ab0032b21fdf12fc3b4fa2729a20467e14f
yujun4464 <147383236+yujun4464@users.noreply.github.com>
2023-12-19 14:52:04 +0800

    删除图像秘密&&使用英语（#199）
    * delete image secret && use english
    
    * node to domain
    
    * volumeMount kuscia-var-tmp

--- scripts/deploy/generate_rsa_key.sh
+++ scripts/deploy/generate_rsa_key.sh
@@ -3,4 +3,4 @@ set -e
 GREEN='\033[0;32m'
 NC='\033[0m'
 KEY=$(openssl genrsa 2048 2>/dev/null | base64 | tr -d "\n" && echo)
-echo -e "${GREEN}生成节点私钥配置:\n\n$KEY\n${NC}"
+echo -e "${GREEN}Generate domain private key configuration:\n\n$KEY\n${NC}"
