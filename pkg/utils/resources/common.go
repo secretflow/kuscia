36d4191a718e7431633d3f5f3b37649a434da917
CLF <46957500+Do-U-Believe-In-Fate@users.noreply.github.com>
2024-04-12 10:13:24 +0800

    【OSCP】feat: kusciaJob support cpu/memory resources config (#268)

--- pkg/utils/resources/common.go
+++ pkg/utils/resources/common.go
@@ -19,6 +19,7 @@ import (
 	"encoding/hex"
 	"errors"
 	"fmt"
+	"reflect"
 	"regexp"
 	"strconv"
 
@@ -27,6 +28,7 @@ import (
 	"github.com/secretflow/kuscia/pkg/common"
 	kusciaapisv1alpha1 "github.com/secretflow/kuscia/pkg/crd/apis/kuscia/v1alpha1"
 	kuscialistersv1alpha1 "github.com/secretflow/kuscia/pkg/crd/listers/kuscia/v1alpha1"
+	k8sresource "k8s.io/apimachinery/pkg/api/resource"
 )
 
 const k3sRegex = `^[a-z0-9]([a-z0-9.-]{0,61}[a-z0-9])?$`
@@ -132,4 +134,42 @@ func HashString(input string) (string, error) {
 	}
 
 	return hex.EncodeToString(hasher.Sum(nil))[:32], nil
+}
+
+// IsEmpty will judge whether data is empty
+func IsEmpty(v interface{}) bool {
+	return reflect.DeepEqual(v, reflect.Zero(reflect.TypeOf(v)).Interface())
+}
+
+// SplitRSC will split the resources into N parts
+func SplitRSC(rsc string, n int) (string, error) {
+	quantity, err := k8sresource.ParseQuantity(rsc)
+	if err != nil {
+		return "", errors.New("failed to parse resource quantity: " + err.Error())
+	}
+	unit := quantity.Format
+	if unit == k8sresource.DecimalSI {
+		quantity.SetMilli(quantity.MilliValue() / int64(n))
+		return quantity.String(), nil
+	} else {
+		bytes := quantity.Value()
+		bytesPerPart := bytes / int64(n)
+		var result string
+		switch {
+		case bytesPerPart >= 1<<60:
+			result = fmt.Sprintf("%.0fPi", float64(bytesPerPart)/(1<<50))
+		case bytesPerPart >= 1<<50:
+			result = fmt.Sprintf("%.0fTi", float64(bytesPerPart)/(1<<40))
+		case bytesPerPart >= 1<<40:
+			result = fmt.Sprintf("%.0fGi", float64(bytesPerPart)/(1<<30))
+		case bytesPerPart >= 1<<30:
+			result = fmt.Sprintf("%.0fMi", float64(bytesPerPart)/(1<<20))
+		case bytesPerPart >= 1<<20:
+			result = fmt.Sprintf("%.0fKi", float64(bytesPerPart)/(1<<10))
+		default:
+			quantity.Set(bytesPerPart)
+			result = quantity.String()
+		}
+		return result, nil
+	}
 }
