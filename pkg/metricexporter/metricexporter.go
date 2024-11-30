515db0b511a97a32b15acb2599b77aebeb0fb809
renji555 <3220102337@zju.edu.cn>
2024-11-30 14:29:31 +0800

    others‘ change

--- pkg/metricexporter/metricexporter.go
+++ pkg/metricexporter/metricexporter.go
@@ -16,20 +16,61 @@ package metricexporter
 
 import (
 	"context"
+	"fmt"
 	"io/ioutil"
 	"net/http"
+	"strconv"
 	"sync"
 	"time"
 
+	"github.com/secretflow/kuscia/pkg/agent/pod"
+	"github.com/secretflow/kuscia/pkg/common"
 	"github.com/secretflow/kuscia/pkg/utils/nlog"
 )
 
 var (
-	ReadyChan = make(chan struct{})
+	ReadyChan  = make(chan struct{})
+	podManager pod.Manager
 )
 
-func getMetrics(url string) ([]byte, error) {
-	request, err := http.NewRequest("GET", url, nil)
+func ListPodMetricUrls(podManager pod.Manager) (map[string]string, error) {
+	if podManager == nil {
+		return nil, fmt.Errorf("podManager is not initialized")
+	}
+
+	metricUrls := map[string]string{}
+	pods := podManager.GetPods()
+
+	for _, pod := range pods {
+		metricPath := ""
+		metricPort := ""
+
+		if val, ok := pod.Annotations[common.MetricPathAnnotationKey]; ok {
+			metricPath = val
+		}
+
+		for _, container := range pod.Spec.Containers {
+			for _, port := range container.Ports {
+				if port.Name == pod.Annotations[common.MetricPortAnnotationKey] {
+					metricPort = strconv.Itoa(int(port.ContainerPort))
+					break
+				}
+			}
+		}
+
+		if metricPath != "" && metricPort != "" {
+			if metricPort == "80" {
+				metricUrls[pod.Name] = fmt.Sprintf("http://%s/%s", pod.Status.PodIP, metricPath)
+			} else {
+				metricUrls[pod.Name] = fmt.Sprintf("http://%s:%s/%s", pod.Status.PodIP, metricPort, metricPath)
+			}
+		}
+	}
+	return metricUrls, nil
+}
+
+func getMetrics(fullURL string) ([]byte, error) {
+	request, err := http.NewRequest("GET", fullURL, nil)
 	if err != nil {
 		nlog.Error("Error creating request:", err)
 		return nil, err
@@ -50,23 +91,22 @@ func getMetrics(url string) ([]byte, error) {
 	}
 	return responseBody, nil
 }
-func metricHandler(metricUrls map[string]string, w http.ResponseWriter) {
-	metricsChan := make(chan []byte, len(metricUrls))
+func metricHandler(metricURLs map[string]string, w http.ResponseWriter) {
+	metricsChan := make(chan []byte, len(metricURLs))
 	var wg sync.WaitGroup
 
-	for key, url := range metricUrls {
+	for _, fullURL := range metricURLs {
 		wg.Add(1)
-		go func(key string, url string) {
+		go func(fullURL string) {
 			defer wg.Done()
 
-			metrics, err := getMetrics(url)
+			metrics, err := getMetrics(fullURL)
 			if err == nil {
 				metricsChan <- metrics
 			} else {
-				nlog.Warnf("metrics[%s] query failed", key)
 				metricsChan <- nil // empty metrics
 			}
-		}(key, url)
+		}(fullURL)
 	}
 
 	go func() {
@@ -83,14 +123,35 @@ func metricHandler(metricUrls map[string]string, w http.ResponseWriter) {
 	}
 }
 
+func combine(map1, map2 map[string]string) map[string]string {
+	for k, v := range map2 {
+		map1[k] = v
+	}
+	return map1
+}
+
 func MetricExporter(ctx context.Context, metricURLs map[string]string, port string) {
-	nlog.Info("Start to export metrics...")
+	nlog.Infof("Start to export metrics on port %s...", port)
+
+	if podManager != nil {
+		podMetrics, err := ListPodMetricUrls(podManager)
+		if err != nil {
+			nlog.Errorf("Error retrieving pod metrics: %v", err)
+		} else {
+			metricURLs = combine(metricURLs, podMetrics)
+		}
+	} else {
+		nlog.Warn("podManager is nil, skipping ListPodMetricUrls call")
+	}
+
 	metricServer := http.NewServeMux()
 	metricServer.HandleFunc("/metrics", func(w http.ResponseWriter, r *http.Request) {
 		metricHandler(metricURLs, w)
 	})
 
 	go func() {
+		nlog.Infof("Starting metric server on port %s", port)
+
 		if err := http.ListenAndServe("0.0.0.0:"+port, metricServer); err != nil {
 			nlog.Error("Fail to start the metric exporterserver", err)
 		}
