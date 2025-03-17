660414457cc80915330ce788289bc3476370cf63
Wangzy <wangziyuan455@Gmail.com>
2025-03-17 16:18:12 +0800

    Add unit tests for pkg/web packages (#520)

--- pkg/web/metrics/ginprometheus_test.go
+++ pkg/web/metrics/ginprometheus_test.go
@@ -19,24 +19,6 @@ func TestRegisterMetricsWithDefault(t *testing.T) {
 	assert.NotNil(t, metrics.resSz)
 }
 
-func TestRegistGin(t *testing.T) {
-	subsystem := "test"
-	e := gin.New()
-	assert.NoError(t, RegistGin(subsystem, e))
-}
-
-func TestRegistGinWithRouter(t *testing.T) {
-	subsystem := "test"
-	e := gin.New()
-	metricsPath := "/metrics"
-	assert.NoError(t, RegistGinWithRouter(subsystem, e, metricsPath))
-
-	req := httptest.NewRequest(http.MethodGet, metricsPath, nil)
-	w := httptest.NewRecorder()
-	e.ServeHTTP(w, req)
-	assert.Equal(t, http.StatusOK, w.Code)
-}
-
 func TestRegistGinWithRouterAuth(t *testing.T) {
 	subsystem := "test"
 	e := gin.New()
@@ -56,48 +38,6 @@ func TestRegistGinWithRouterAuth(t *testing.T) {
 	w = httptest.NewRecorder()
 	e.ServeHTTP(w, req)
 	assert.Equal(t, http.StatusOK, w.Code)
-}
-
-func TestHandlerFunc(t *testing.T) {
-	subsystem := "test"
-	metrics, err := registerMetricsWithDefault(subsystem)
-	assert.NoError(t, err)
-
-	e := gin.New()
-	e.Use(handlerFunc(metrics, ""))
-
-	e.GET("/test", func(c *gin.Context) {
-		c.Status(http.StatusOK)
-	})
-	e.POST("/test", func(c *gin.Context) {
-		c.Status(http.StatusOK)
-	})
-
-	tests := []struct {
-		name     string
-		give     string
-		wantCode int
-	}{
-		{
-			name:     "GET request",
-			give:     "/test",
-			wantCode: http.StatusOK,
-		},
-		{
-			name:     "POST request",
-			give:     "/test",
-			wantCode: http.StatusOK,
-		},
-	}
-
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			req := httptest.NewRequest(http.MethodGet, tt.give, nil)
-			w := httptest.NewRecorder()
-			e.ServeHTTP(w, req)
-			assert.Equal(t, tt.wantCode, w.Code)
-		})
-	}
 }
 
 func TestPrometheusHandler(t *testing.T) {
