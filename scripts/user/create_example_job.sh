ca97bb07f91fcf09623a8abc97383ac5aa0f4e31
bzzbzz7 <fyzy7@qq.com>
2025-03-25 19:48:23 +0800

    Revert "修复在bob侧执行测试任务时，获取domainId错误"
    This reverts commit e77112acc67f5d006580f757224c16a28ec5ecb1.

--- scripts/user/create_example_job.sh
+++ scripts/user/create_example_job.sh
@@ -41,7 +41,7 @@ fi
 ROOT=$(cd "$(dirname "${BASH_SOURCE[0]}")/../.." && pwd -P)
 pushd ${ROOT} || exit
 
-SELF_DOMAIN_ID=$(grep -E "^domainID:" "/home/kuscia/etc/conf/kuscia.yaml" | awk '{ print $2 }' | sed 's/"//g' | tr -d '\r\n')
+SELF_DOMAIN_ID=$(grep "domainID:" "/home/kuscia/etc/conf/kuscia.yaml" | awk '{ print $2 }' | sed 's/"//g' | tr -d '\r\n')
 
 INITIATOR=alice
 if [[ $SELF_DOMAIN_ID == bob ]]; then
